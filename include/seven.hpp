6490d439dd770c6b0247fbf09cf568cdca2d01b6
BlackWLN <maxobana@gmail.com>
2025-10-24 22:10:02 +0300

    first commit

---
+++ include/seven.hpp
@@ -0,0 +1,34 @@
+#ifndef SEVEN_H
+#define SEVEN_H
+
+#include <cstddef>
+#include <initializer_list>
+#include <iosfwd>
+#include <stdexcept>
+#include <string>
+
+class Seven final {
+public:
+  Seven();
+  Seven(const size_t &n, unsigned char defaultVal = 0);
+  Seven(const std::initializer_list<unsigned char> &initVals);
+  Seven(const std::string &srcStr);
+  Seven(const Seven &other);
+  Seven(Seven &&other) noexcept;
+  Seven &operator=(const Seven &other);
+  Seven &operator=(Seven &&other) noexcept;
+  ~Seven() noexcept;
+
+  Seven operator+(const Seven &other) const;
+  Seven operator-(const Seven &other) const;
+  bool operator==(const Seven &other) const;
+  bool operator>=(const Seven &other) const;
+
+  friend std::ostream &operator<<(std::ostream &os, const Seven &f);
+
+private:
+  unsigned char *data;
+  size_t size;
+};
+
+#endif // FOUR_H
